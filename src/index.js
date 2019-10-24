55eea661ee7cbce4b5dc438c53c0da4ae514dd1d
Dylan Cruse <dylancruse@live.com>
2019-10-24 16:05:41 -0500

    Install apollo-link-error for application level error handling, add component error handling to Profile

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import * as serviceWorker from './serviceWorker';
@@ -10,8 +12,21 @@ import App from './App';
 
 import './style.css';
 
-const GITHUB_BASE_URL = 'https://api.github.com/graphql';
 
+// Application level error handling with Apollo
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  // TODO Add actual error handling for these
+  if (graphQLErrors) {
+    console.log('GraphQL Error!');
+  }
+
+  if (networkError) {
+    console.log('Network Error!');
+  }
+});
+
+// Define the GraphQL endpoint to be used by Apollo Client
+const GITHUB_BASE_URL = 'https://api.github.com/graphql';
 const httpLink = new HttpLink({
   uri: GITHUB_BASE_URL,
   headers: {
@@ -21,10 +36,18 @@ const httpLink = new HttpLink({
   },
 });
 
+// Combine links to be used to configure Apollo Client
+// and add functionality. Links access and modify GraphQL
+// control flow. httpLink is a terminating link so it must
+// be placed last. 
+const link = ApolloLink.from([errorLink, httpLink]);
+
+// Cache where data is managed by Apollo Client
+// (Normalizes data, caches requests, read/write)
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache,
 });
 
