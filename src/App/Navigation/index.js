9c7e64e75cc95a83e982084e6189e2afca8edafc
Dylan Cruse <dylancruse@live.com>
2019-10-27 20:57:28 -0500

    Add search to navbar to run a new dynamic graphql query

--- src/App/Navigation/index.js
+++ src/App/Navigation/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
+import Button from '../../Button';
+import Input from '../../Input';
 import * as routes from '../../constants/routes';
 
 import './style.css';
 
-const Navigation = () => (
+const Navigation = ({
+  location: { pathname },
+  organizationName,
+  onOrganizationSearch,
+}) => (
   <header className="Navigation">
     <div className="Navigation-link">
       <Link to={routes.PROFILE}>Profile</Link>
@@ -13,7 +19,49 @@ const Navigation = () => (
     <div className="Navigation-link">
       <Link to={routes.ORGANIZATION}>Organization</Link>
     </div>
+
+    {pathname === routes.ORGANIZATION && (
+      <OrganizationSearch 
+        organizationName={organizationName}
+        onOrganizationSearch={onOrganizationSearch}
+      />
+    )}
   </header>
 );
 
-export default Navigation;
+class OrganizationSearch extends React.Component {
+  state = {
+    value: this.props.organizationName
+  };
+
+  onChange = event => {
+    this.setState({ value: event.target.value })
+  };
+
+  onSubmit = event => {
+    this.props.onOrganizationSearch(this.state.value);
+    event.preventDefault();
+  };
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div className="Navigation-search">
+        <form onSubmit={this.onSubmit}>
+          <Input 
+            color={'white'}
+            type="text"
+            value={value}
+            onChange={this.onChange}
+          />{' '}
+          <Button color={'white'} type="submit">
+            Search
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Navigation);
--- src/App/index.js
+++ src/App/index.js
@@ -10,12 +10,24 @@ import * as routes from '../constants/routes';
 import './style.css';
 
 class App extends Component {
+  state = {
+    organizationName: 'the-road-to-learn-react',
+  }
+
+  onOrganizationSearch = value => {
+    this.setState({ organizationName: value});
+  };
+
   render() {
+    const { organizationName } = this.state;
+
     return (
       <Router>
         <div className="App">
-
-          <Navigation />
+          <Navigation 
+            organizationName={organizationName}
+            onOrganizationSearch={this.onOrganizationSearch}
+          />
 
           <div className="App-main">
             <Route 
@@ -23,7 +35,7 @@ class App extends Component {
               component={() => (
                 <div className="App-content_large-header">
                   <Organization 
-                    organizationName={'the-road-to-learn-react'}
+                    organizationName={organizationName}
                   />
                 </div> 
               )}
