3166baefa9dbe33dae75a45f57b710ed8aabb76f
Dylan Cruse <dylancruse@live.com>
2019-10-29 20:27:10 -0500

    Format date

--- src/Comments/CommentItem/index.js
+++ src/Comments/CommentItem/index.js
@@ -7,15 +7,25 @@ const CommentItem = ({
   body,
   commit,
   publishedAt,
-}) => (
-  <div className="CommentItem">
-    <div className="CommentItem-info">
-      <p><span className="CommentItem-info-span">{'User: '}</span>{author.login}</p>
-      <p><span className="CommentItem-info-span">{'Commit: '}</span>{commit.message}</p>
-      <p><span className="CommentItem-info-span">{'Published At: '}</span>{publishedAt}</p>
+}) => {
+  const date = new Date(publishedAt);
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+
+  return (
+    <div className="CommentItem">
+      <div className="CommentItem-info">
+        <p><span className="CommentItem-info-span">{'User: '}</span>{author.login}</p>
+        <p><span className="CommentItem-info-span">{'On Commit: '}</span>{commit.message}</p>
+        <p>
+          <span className="CommentItem-info-span">{'Created: '}</span>
+          {month}{'-'}{day}{'-'}{year}
+        </p>
+      </div>
+      <p><span className="CommentItem-info-span">{'Text: '}</span>{body}</p>
     </div>
-    <p><span className="CommentItem-info-span">{'Text: '}</span>{body}</p>
-  </div>
-);
+  );
+};
 
 export default CommentItem;
